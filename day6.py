95c77cd5663a82b2d33c006cb2cc569fa93c83a8
afnan <afnanalshakrah2@hotmail.com>
2019-08-25 15:55:05 +0300

    this is my code
    Co-Authored-By: SaudiDevOrg <saudidevorg@users.noreply.github.com>

---
+++ day6.py
@@ -0,0 +1,34 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x= int(1)
+>>> y= int(2.8)
+>>> z= int("3")
+>>> print(x)
+1
+>>> print(y)
+2
+>>> print(z)
+3
+>>> x= float(1)
+>>> y= float(2.8)
+>>> z= float("3")
+>>> w= float("4.3")
+>>> print(x)
+1.0
+>>> print(y)
+2.8
+>>> print(z)
+3.0
+>>> print(w)
+4.3
+>>> x= str("s1")
+>>> y= str(2)
+>>> z= str(3.0)
+>>> print(x)
+s1
+>>> print(y)
+2
+>>> print(z)
+3.0
+>>> 
