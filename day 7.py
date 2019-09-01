32d32b89dad476c24c6a35db4860d64ccb9ddd89
afnan <afnanalshakrah2@hotmail.com>
2019-09-01 19:17:32 +0300

    Create day 7.py
    this my code
    
    Co-Authored-By: SaudiDevOrg <saudidevorg@users.noreply.github.com>

---
+++ day 7.py
@@ -0,0 +1,26 @@
+Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> print('hello')
+hello
+>>> a = "hello"
+>>> print(a)
+hello
+>>> a = """Loren ipesm dolor sit amet,
+consecture adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labor et dolore magna aliqua"""
+>>> print(a)
+Loren ipesm dolor sit amet,
+consecture adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labor et dolore magna aliqua
+>>> a= "hello world"
+>>> print(a[1])
+e
+>>> b= "hello world"
+>>> print(b[2:5])
+llo
+>>> 
